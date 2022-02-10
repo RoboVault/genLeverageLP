766165ce34369f1b2b5123475056daf823de63b8
degen <robovault1234@gmail.com>
2022-02-10 22:17:58 +0700

    test with loop

--- tests/test_harvest.py
+++ tests/test_harvest.py
@@ -18,25 +18,27 @@ def test_profitable_harvest(
     assert pytest.approx(strategy.estimatedTotalAssets(), rel=RELATIVE_APPROX) == amount
     before_pps = vault.pricePerShare()
 
-    # Use a whale of the harvest token to send
-    harvest = interface.ERC20(conf['harvest_token'])
-    harvestWhale = accounts.at(conf['harvest_token_whale'], True)
-    sendAmount = round((vault.totalAssets() / conf['harvest_token_price']) * 0.05)
-    print('Send amount: {0}'.format(sendAmount))
-    print('harvestWhale balance: {0}'.format(harvest.balanceOf(harvestWhale)))
-    harvest.transfer(strategy, sendAmount, {'from': harvestWhale})
+    for i in range(4):
 
-    # Harvest 2: Realize profit
-    chain.sleep(1)
-    tx = strategy.harvest()
-    chain.sleep(3600 * 6)  # 6 hrs needed for profits to unlock
-    chain.mine(1)
-    profit = token.balanceOf(vault.address)  # Profits go to vault
+        # Use a whale of the harvest token to send
+        harvest = interface.ERC20(conf['harvest_token'])
+        harvestWhale = accounts.at(conf['harvest_token_whale'], True)
+        sendAmount = round((vault.totalAssets() / conf['harvest_token_price']) * 0.0125)
+        print('Send amount: {0}'.format(sendAmount))
+        print('harvestWhale balance: {0}'.format(harvest.balanceOf(harvestWhale)))
+        harvest.transfer(strategy, sendAmount, {'from': harvestWhale})
 
-    print('Price per Share :  {0}'.format(vault.pricePerShare()))
-    print('Estimated Assets :  {0}'.format(strategy.estimatedTotalAssets()))
-    #assert strategy.estimatedTotalAssets() + profit > amount
-    #assert vault.pricePerShare() > before_pps
+        # Harvest 2: Realize profit
+        chain.sleep(1)
+        tx = strategy.harvest()
+        chain.sleep(3600 * 6)  # 6 hrs needed for profits to unlock
+        chain.mine(1)
+        profit = token.balanceOf(vault.address)  # Profits go to vault
+
+        print('Price per Share :  {0}'.format(vault.pricePerShare()))
+        print('Estimated Assets :  {0}'.format(strategy.estimatedTotalAssets()))
+    assert strategy.estimatedTotalAssets() + profit > amount
+    assert vault.pricePerShare() > before_pps
 
 
 def test_profitable_harvest_trading_fees(
@@ -59,52 +61,54 @@ def test_profitable_harvest_trading_fees(
 
     tradingFeeWhale = '0xd061c6586670792331E14a80f3b3Bb267189C681'
 
+
     shortA = Contract(strategy.shortA())
     shortB = Contract(strategy.shortB())
     sendAmtA = shortA.balanceOf(lp_token)*0.025
     sendAmtB = shortB.balanceOf(lp_token)*0.025
 
+    for i in range(4):
 
-    shortA.transfer(lp_token, sendAmtA, {'from': tradingFeeWhale})
-    shortB.transfer(lp_token, sendAmtB, {'from': tradingFeeWhale})
+        shortA.transfer(lp_token, sendAmtA, {'from': tradingFeeWhale})
+        shortB.transfer(lp_token, sendAmtB, {'from': tradingFeeWhale})
 
-    spookyRouter = Contract("0xF491e7B69E4244ad4002BC14e878a34207E38c29")
+        spookyRouter = Contract("0xF491e7B69E4244ad4002BC14e878a34207E38c29")
 
-    shortA.approve(spookyRouter, 2**256-1, {"from": tradingFeeWhale})
-    swapAmt = sendAmtA*0.01 
-    # do tiny trade as this should make sure reserves are updated 
-    spookyRouter.swapExactTokensForTokens(swapAmt, 0, [shortA, shortB], tradingFeeWhale, 2**256-1, {"from": tradingFeeWhale})
-
-
-    # Harvest 2: Realize profit
-    chain.sleep(1)
-    chain.mine(1)
-
-    print('Pre Harvest')
-    print('debt Ratio A :  {0}'.format(strategy.calcDebtRatioA()))
-    print('debt Ratio B :  {0}'.format(strategy.calcDebtRatioB()))
-    print('CollatRatio: {0}'.format(strategy.calcCollateral()))
-
-    print("-----")
-    print('Price per Share :  {0}'.format(vault.pricePerShare()))
-    print('Estimated Assets :  {0}'.format(strategy.estimatedTotalAssets()))
-
-    print("Sell Trading Fees & Complete Harvest")
-    strategy.sellTradingFees()
-    strategy.harvest()
-    
-    chain.sleep(3600 * 6)  # 6 hrs needed for profits to unlock
-    chain.mine(1)
-    profit = token.balanceOf(vault.address)  # Profits go to vault
-
-    print('After Harvest')
-    print('debt Ratio A :  {0}'.format(strategy.calcDebtRatioA()))
-    print('debt Ratio B :  {0}'.format(strategy.calcDebtRatioB()))
-    print('CollatRatio: {0}'.format(strategy.calcCollateral()))
-    print('Price per Share :  {0}'.format(vault.pricePerShare()))
-    print('Estimated Assets :  {0}'.format(strategy.estimatedTotalAssets()))
+        shortA.approve(spookyRouter, 2**256-1, {"from": tradingFeeWhale})
+        swapAmt = sendAmtA*0.01 
+        # do tiny trade as this should make sure reserves are updated 
+        spookyRouter.swapExactTokensForTokens(swapAmt, 0, [shortA, shortB], tradingFeeWhale, 2**256-1, {"from": tradingFeeWhale})
 
 
-    #assert strategy.estimatedTotalAssets() + profit > amount
-    #assert vault.pricePerShare() > before_pps
+        # Harvest 2: Realize profit
+        chain.sleep(1)
+        chain.mine(1)
+
+        print('Pre Harvest')
+        print('debt Ratio A :  {0}'.format(strategy.calcDebtRatioA()))
+        print('debt Ratio B :  {0}'.format(strategy.calcDebtRatioB()))
+        print('CollatRatio: {0}'.format(strategy.calcCollateral()))
+
+        print("-----")
+        print('Price per Share :  {0}'.format(vault.pricePerShare()))
+        print('Estimated Assets :  {0}'.format(strategy.estimatedTotalAssets()))
+
+        print("Sell Trading Fees & Complete Harvest")
+        strategy.sellTradingFees()
+        strategy.harvest()
+        
+        chain.sleep(3600 * 6)  # 6 hrs needed for profits to unlock
+        chain.mine(1)
+        profit = token.balanceOf(vault.address)  # Profits go to vault
+
+        print('After Harvest')
+        print('debt Ratio A :  {0}'.format(strategy.calcDebtRatioA()))
+        print('debt Ratio B :  {0}'.format(strategy.calcDebtRatioB()))
+        print('CollatRatio: {0}'.format(strategy.calcCollateral()))
+        print('Price per Share :  {0}'.format(vault.pricePerShare()))
+        print('Estimated Assets :  {0}'.format(strategy.estimatedTotalAssets()))
+
+
+    assert strategy.estimatedTotalAssets() + profit > amount
+    assert vault.pricePerShare() > before_pps
 
